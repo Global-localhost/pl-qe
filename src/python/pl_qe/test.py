e2f5fc97da4e15edcc11252d4491c7f6ddb14a37
co9olguy <co9olguy@users.noreply.github.com>
2020-04-15 16:05:59 -0400

    moving away from about due to import errors

--- src/python/pl_qe/test.py
+++ src/python/pl_qe/test.py
@@ -19,3 +19,24 @@ def about():
 
     with open("about.json",'w') as f:
         f.write(json.dumps(message_dict, indent=2))
+        
+
+def simple_circuit():
+    dev = qml.device('default.qubit', wires=2)
+    
+    @qml.qnode(dev)
+    def circuit(x):
+        qml.RX(x, wires=0)
+        qml.RY(x, wires=1)
+        qml.CNOT(wires=[0,1])
+        return qml.expval(qml.PauliZ(0))
+        
+    p = 0.123
+    output = circuit(p)
+    
+    message_dict = {}
+    message_dict["message"] = "{}".format(output)
+    message_dict["schema"] = "message"
+
+    with open("circuit_output.json",'w') as f:
+        f.write(json.dumps(message_dict, indent=2))
