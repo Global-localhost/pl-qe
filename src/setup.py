b7697b2c111d6f53b77c981ffff7de2b5d0a1262
Josh Izaac <josh146@gmail.com>
2020-04-16 20:16:16 +0930

    black

--- src/setup.py
+++ src/setup.py
@@ -1,17 +1,14 @@
 import setuptools
 
-requirements = ['pip', 'pennylane @ git+https://github.com/XanaduAI/pennylane@remove_about#egg=pennylane'
-    ]
+requirements = [
+    "pip",
+    "pennylane @ git+https://github.com/XanaduAI/pennylane@remove_about#egg=pennylane",
+]
 
 setuptools.setup(
-    name                            = "pl_qe",
-    packages                        = setuptools.find_packages(where = "python"),
-    package_dir                     = {
-        "" : "python"
-    },
-    #install_requires                = requirements,
-    classifiers                     = (
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ),
+    name="pl_qe",
+    packages=setuptools.find_packages(where="python"),
+    package_dir={"": "python"},
+    # install_requires                = requirements,
+    classifiers=("Programming Language :: Python :: 3", "Operating System :: OS Independent",),
 )
